af056c55d3ce6985fc41ef6650a2b66ded751f4c
William Purcell <williampurcell@Williams-MBP.home>
2023-11-27 23:48:31 +0100

    have a game playing field built

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React, { useState, useEffect } from 'react'
+import Player from './Player'
+
 function App() {
+
+  let [game, setGame] = useState ({
+    isStarted: false,
+    isOver: false,
+    isDraw: false
+  })
+  let [grid, setGrid] = useState ({
+    selected: []
+  })
+  let [player1, setPlayer1] = useState(new Player('will', 'green'))
+  let [player2, setPlayer2] = useState(new Player('jeb', 'blue'))
+
+  console.log(player1, player2)
+
+  // if game hasnt started, render start screen
+  // if game has started, render grid
+  // if game is over, render game over
+  // // maybe render a game over thing and wait a few seconds before 
+  // // swapping 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="just-lines">
+        <div className="grid-container">
+          <div className="grid-square" id="one">1</div>
+          <div className="grid-square" id="two">2</div>
+          <div className="grid-square" id="three">3</div>
+          <div className="grid-square" id="four">4</div>
+          <div className="grid-square" id="five">5</div>
+          <div className="grid-square" id="six">6</div>
+          <div className="grid-square" id="seven">7</div>
+          <div className="grid-square" id="eight">8</div>
+          <div className="grid-square" id="nine">9</div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Player.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Player (name, color) {
+    this.name = name
+    this.color = color
+    this.one = false
+    this.two = false
+    this.three = false
+    this.four = false
+    this.five = false
+    this.six = false
+    this.seven = false
+    this.eight = false
+    this.nine = false
+}
