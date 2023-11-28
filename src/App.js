1ab4b507fe124e2b8025a77e373cf3a08acfaeef
William Purcell <williampurcell@Williams-MBP.home>
2023-11-28 22:08:06 +0100

    gotta figure out how to add emojis

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ function App() {
   let [game, setGame] = useState ({
     isStarted: false,
     isOver: false,
-    isDraw: false
+    isDraw: false,
+    playerTurn: 'will'
   })
   let [grid, setGrid] = useState ({
     one: '',
@@ -22,34 +23,57 @@ function App() {
     eight: '',
     nine: '',
   })
-  let [playerOne, setPlayerOne] = useState(new Player('will', 'green', 'X', true))
-  let [playerTwo, setPlayerTwo] = useState(new Player('jeb', 'blue', 'O', false))
+  let [playerOne, setPlayerOne] = useState(new Player('will', 'green', `🏀`, true))
+  let [playerTwo, setPlayerTwo] = useState(new Player('jeb', 'blue', `🌴`, false))
   let [turn, setTurn] = useState(0)
 
-  console.log(playerOne, playerTwo)
-
-  console.log(grid)
-
-  // if the space isn't taken, update the appropriate player and the grid,
-  // and increment turn. if the space is taken, do nothing
+  // -- click handler for grid squares
+  // if the space isn't taken, update player object to give them the square,
+  // add the symbol of that player to the space they selected, and update
+  // whose turn it is. then, increment turn. 
+  // if the space is taken, do nothing
   let clickHandle = (e) => {
     if (grid[e.target.id] === '') {
       if (turn % 2 === 0) {
         setPlayerOne({...playerOne, [e.target.id]: true})
         setGrid({...grid, [e.target.id]: playerOne.symbol})
+        setGame({...game, playerTurn: playerTwo.name})
       } else if (turn % 2 === 1) {
         setPlayerTwo({...playerTwo, [e.target.id]: true})
         setGrid({...grid, [e.target.id]: playerTwo.symbol})
+        setGame({...game, playerTurn: playerOne.name})
       }
       setTurn(turn += 1)
     } else {}
   }
 
+  // -- checks for win
+  // accepts a player object and checks for true on all winning permutations
+  // for that player
+  let checkWin = (player) => {
+    let { one, two, three, four, five, six, seven, eight, nine} = player
+
+    if ( (one && two && three) || (four && five && six) || (seven && eight && nine) || // check rows
+      (one && four && seven) || (two && five && eight ) || (three && six && nine ) || // check columns
+      (one && five && nine) || (seven && five && three) ) { // check diagonals
+        console.log(`win for ${player.name}`)
+    }
+  }
+
+  // -- useEffect on turn state
   // every time there's a new turn, we want to check to see if someone
   // won the game
+  // PLEASE NOTE: dealt with a weird bug where calling checkWin() in the
+  // click handler was firing before the player object was updated.
+  // Moved checkWin() into this useEffect, and reversed the % logic because 
+  // at this point turn has already been incremented by 1
   useEffect(() => {
-    let { one, two, three, four, five, six, seven, eight, nine} = grid
-    if ( (one === two === three) || (four === five === six) ) {}
+    if (turn % 2 === 1)
+      checkWin(playerOne)
+    else if (turn % 2 === 0)
+      checkWin(playerTwo)
+    else
+      console.log(`somethin else wrong :cringe:`)
   }, [turn])
 
   // if game hasnt started, render start screen
@@ -57,11 +81,13 @@ function App() {
   // if game has started, render grid
   return (
     <div className="App">
+      <div className="header-container">
+        <div className="game-name"><p>Tic Tac Toe</p></div>
+        <div className="game-status"><p>It's {game.playerTurn}'s Turn</p></div>
+      </div>
       <div className="just-lines">
         <div className="grid-container">
-          {Object.keys(grid).map(number => {
-            return <GridSquare style='grid-square' id={number} value={grid[number]} click={clickHandle}/>
-          })}
+          {Object.keys(grid).map(number => <GridSquare style='grid-square' id={number} value={grid[number]} click={clickHandle}/>)}
         </div>
       </div>
     </div>
